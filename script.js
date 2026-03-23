dc27c8860f4547ba9f625e2a970601ca569fc29a
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-03-23 18:57:40 +0100

    fix(tasks): handle no due date scenario in task display

--- script.js
+++ script.js
@@ -335,7 +335,9 @@ function updateHTML(taskTitleValue, taskDescriptionValue, priorityItemValue, dat
         <div class="taskDesc">${taskDescriptionValue}</div>
         <div class="taskFooter column">
             <div class="priority" style="color: ${color};">${priorityItemValue}</div>
-            <div class="dueDate ${dateInputValue === 'Today' || dateInputValue === 'Overdue' ? 'red' : ''}">Due: ${new Date(dateInputValue).toDateString()}</div>
+            <div class="dueDate ${dateInputValue === 'Today' || dateInputValue === 'Overdue' ? 'red' : ''}">
+                Due: ${dateInputValue === '--' ? 'No due date' : new Date(dateInputValue).toDateString()}
+            </div>
         </div>
     </div>
     `;
